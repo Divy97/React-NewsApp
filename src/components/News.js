b09773b2d542a460cbc254dc06eed48e47af7cbd
Divy Parekh <divyparekh1810@gmail.com>
2022-05-11 19:21:38 +0530

    Project initialized and basic componenet structure is done

--- src/components/News.js
+++ src/components/News.js
@@ -4,9 +4,19 @@ import NewsItem from './NewsItem'
 export default class News extends Component {
   render() {
     return (
-      <div>
-          News
-          <NewsItem />
+      <div className='container my-3'>
+          <h2>NewsBuddy- Top headlines</h2>
+          <div className='row'>
+              <div className='col-md-4'>
+                <NewsItem title="myTitle" description="MyDescription"/>
+              </div>
+              <div className='col-md-4'>
+                <NewsItem title="myTitle" description="MyDescription"/>
+              </div>
+              <div className='col-md-4'>
+                <NewsItem title="myTitle" description="MyDescription"/>
+              </div>              
+          </div> 
       </div>
     )
   }
