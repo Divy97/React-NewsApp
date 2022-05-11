47fc96ef934b1b97745f753e179205ba6aa4cbb9
Divy Parekh <divyparekh1810@gmail.com>
2022-05-11 22:27:14 +0530

    Added Previous & Next Buttons to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -2,53 +2,13 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
-    articles = [
-        {
-            "source": {
-                "id": "bbc-sport",
-                "name": "BBC Sport"
-            },
-            "author": "BBC Sport",
-            "title": "Shane Warne memorial - watch & follow updates",
-            "description": "Watch live coverage and follow text updates and tributes from the state memorial for Australian cricket legend Shane Warne at the Melbourne Cricket Ground.",
-            "url": "http://www.bbc.co.uk/sport/live/cricket/60916236",
-            "urlToImage": "https:////m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png",
-            "publishedAt": "2022-03-30T08:22:26.498888Z",
-            "content": "Former England bowler and BBC cricket presenter Isa Guha, who became a colleague of Warne's in the commentary box: It has been a strange few weeks - a lot of shock and then we did our own tribute at… [+396 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ];
     constructor(){
         super();
         console.log("This is constructor of news component");
         this.state={
-            articles: this.articles,
-            loading: false
+            articles: [],
+            loading: false,
+            page: 1
         }
     }
 
@@ -57,7 +17,38 @@ export default class News extends Component {
         let data = await fetch(url);
         let parseData = await data.json();
         console.log(parseData); 
-        this.setState({articles: parseData.articles})
+        this.setState({articles: parseData.articles, totalResults: parseData.totalResults})
+    }
+
+    handlePreviousClick = async () => {
+        console.log("previous");
+
+        
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=da8a69033e81444fa4ca4a2909d0db5a&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parseData = await data.json();
+        console.log(parseData); 
+        this.setState({
+            articles: parseData.articles,
+            page: this.state.page - 1
+        })
+    }
+
+    handleNextClick = async () => {
+        console.log("next");
+
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+        }else{
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=da8a69033e81444fa4ca4a2909d0db5a&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url);
+            let parseData = await data.json();
+            console.log(parseData); 
+            this.setState({
+                articles: parseData.articles,
+                page: this.state.page + 1
+            })
+        }
     }
   render() {
     return (
@@ -70,7 +61,11 @@ export default class News extends Component {
             </div>  
           })}          
           </div> 
+          <div className='container d-flex justify-content-between'>
+              <button disabled={this.state.page<=1} type='button' className='btn btn-dark' onClick={this.handlePreviousClick}>&larr; Previous</button>
+              <button type='button' className='btn btn-dark' onClick={this.handleNextClick}>Next &rarr;</button>
+          </div>
       </div>
-    )
+    ) 
   }
 }
