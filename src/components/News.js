7f6e7e039f7c4291ad60e0a8f8718d6fc70f790a
Divy Parekh <divyparekh1810@gmail.com>
2022-05-12 12:56:34 +0530

    Added time, author and news source to NewsBuddy

--- src/components/News.js
+++ src/components/News.js
@@ -92,6 +92,9 @@ export default class News extends Component {
                   description={element.description}
                   imageUrl={element.urlToImage}
                   newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
                 />
               </div>
             );
